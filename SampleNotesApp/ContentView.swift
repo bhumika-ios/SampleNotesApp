719b97d1c325ac542b642ccf808697feaaf1bcdc
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-09-20 17:01:38 +0530

    add 2 column wise display folder

--- SampleNotesApp/ContentView.swift
+++ SampleNotesApp/ContentView.swift
@@ -10,81 +10,54 @@ import CoreData
 struct ContentView: View {
     @StateObject var viewModel = FolderViewModel()
     @State var folderName: String = ""
-   // @State var companyOwner: String = ""
     @State var showAddBottomSheet = false
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16)
+    ]
+    
     var body: some View {
         NavigationView {
             VStack {
-//                HStack {
-//                    VStack {
-//                        TextField("Folder Name", text: $companyName)
-//                            .font(.headline)
-//                            .padding(.leading)
-//                            .frame(height: 55)
-//                            .background(Color(uiColor: .systemGray5))
-//                            .cornerRadius(5)
-//
-////                        TextField("Enter Owner Name", text: $companyOwner)
-////                            .font(.headline)
-////                            .padding(.leading)
-////                            .frame(height: 55)
-////                            .background(Color(uiColor: .systemGray5))
-////                            .cornerRadius(5)
-//                    }
-//
-//                    Button {
-//                        viewModel.addDataToCoreData(companyTitle: companyName)
-//                        self.companyName = ""
-//                       // self.companyOwner = ""
-//                    } label: {
-//                        Text("Add")
-//                    }
-//
-//                }.padding(.horizontal)
-                
                 ScrollView {
-                    ForEach(viewModel.folderArray, id: \.id) { item in
-                        NavigationLink {
-                         //   EmployeesView(company: item)
-                        } label: {
-                            VStack(alignment: .leading) {
-                                Image(systemName: "folder")
-                                    .resizable()
-                                    .frame(width: 80, height: 70)
-                                Text(item.title ?? "")
-                                    .fontWeight(.semibold)
-                                    .font(.headline)
-//                                Text(item.owner ?? "")
-//                                    .font(.subheadline)
+                    LazyVGrid(columns: columns, spacing: 16) {
+                        ForEach(viewModel.folderArray, id: \.id) { item in
+                            NavigationLink {
+                                // EmployeesView(company: item)
+                            } label: {
+                                VStack(alignment: .leading) {
+                                    Image(systemName: "folder.fill")
+                                        .resizable()
+                                        .frame(width: 130, height: 100)
+                                    Text(item.title ?? "")
+                                        .fontWeight(.semibold)
+                                        .font(.headline)
+                                    // Text(item.owner ?? "")
+                                    // .font(.subheadline)
+                                }
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.horizontal)
+                                .padding(.bottom)
                             }
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.horizontal)
-                            .padding(.bottom)
                         }
-
-                        
                     }
                 }
-                Button{
-                    //taskModel.OpenEditTask.toggle()
+                
+                Button {
                     showAddBottomSheet.toggle()
+                } label: {
+                    Label {
                         
-                    //isAddTodoOpen = true
-                }label: {
-                    Label{
-                        
-                    }icon: {
+                    } icon: {
                         Image(systemName: "plus.app.fill")
-                      //  Image(systemName: "plus")
                             .resizable()
                             .frame(width: 50, height: 50)
                             .padding()
-                        
-                        
                     }
                 }
-                .offset(x:150)
-                .sheet(isPresented: $showAddBottomSheet){
+                .offset(x: 150)
+                .sheet(isPresented: $showAddBottomSheet) {
                     if #available(iOS 16.0, *) {
                         AddFolderView(viewModel: viewModel, folderName: folderName, showAddBottomSheet: $showAddBottomSheet)
                             .presentationDetents([.height(180), .height(180)])
@@ -92,7 +65,8 @@ struct ContentView: View {
                         // Fallback on earlier versions
                     }
                 }
-            }.navigationTitle("Folder")
+            }
+            .navigationTitle("Notes")
         }
     }
 }
