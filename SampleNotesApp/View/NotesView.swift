ff4bd88dde8588392f8757b5b7a608c2e44d3d3a
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-09-21 17:00:32 +0530

    create note view for display adde note particular folder wise

---
+++ SampleNotesApp/View/NotesView.swift
@@ -0,0 +1,86 @@
+//
+//  NotesView.swift
+//  SampleNotesApp
+//
+//  Created by Bhumika Patel on 21/09/23.
+//
+
+import SwiftUI
+import MasonryStack
+
+struct NotesView: View {
+    @ObservedObject var viewModel: NotesViewModel
+    @State var showAddFullSheet = false
+
+    init(folder: Folder) {
+        self.viewModel = NotesViewModel(folder: folder)
+        viewModel.fetchNotes()
+    }
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScrollView(showsIndicators: false) {
+                    MasonryVStack(spacing: 6) {
+                        ForEach(viewModel.notesArray.indices, id: \.self) { index in
+                            let item = viewModel.notesArray[index]
+                            let height = calculateHeight(index: index)
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.blue)
+                                .frame(width: 165, height: height)
+                               // .padding()
+                                .overlay(
+                                    VStack(alignment: .leading) {
+                                        Text(item.unwrappedName)
+                                            .fontWeight(.semibold)
+                                            .font(.headline)
+                                        Text(item.folder?.title ?? "")
+                                            .font(.subheadline)
+                                    }
+                                    .padding()
+                                )
+                                .onTapGesture {
+                                    // Handle tapping on a note
+                                }
+                        }
+                    }
+
+                  //  .padding()
+                }
+
+                Button {
+                    showAddFullSheet.toggle()
+                } label: {
+                    Image(systemName: "plus.app.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                }
+                .offset(x: 150)
+                .fullScreenCover(isPresented: $showAddFullSheet) {
+                    if #available(iOS 16.0, *) {
+                        AddNotesView(viewModel: viewModel, showAddFullSheet: $showAddFullSheet)
+                    } else {
+                        // Fallback on earlier versions
+                    }
+                }
+               
+            }
+            .padding()
+           
+        }
+    }
+
+    func calculateHeight(index: Int) -> CGFloat {
+            let repeatingHeights: [CGFloat] = [300, 150, 200, 250] // Heights for the first four notes
+            
+            let repeatingPatternIndex = index % repeatingHeights.count
+            let minHeight: CGFloat = 100 // Minimum height for smaller notes
+            
+            // Use the larger height if it's greater than the minimum height, otherwise use the minimum height
+            return max(repeatingHeights[repeatingPatternIndex], minHeight)
+        }
+}
